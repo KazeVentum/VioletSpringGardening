720845a8a108a0a26072ae1a66faa6ecf9d33247
KazeVentum <132288586+KazeVentum@users.noreply.github.com.>
2024-03-14 19:16:00 -0500

    feat: :sparkles: Authentication in login and register in database.

---
+++ violetSpringGardening/page/auth.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtener la URL actual
+  const currentUrl = window.location.pathname;
+
+  if (currentUrl.includes("login.html")) {
+    // Código específico para la página de login
+    // Código específico para la página de login
+    const loginForm = document.getElementById("formUserToken");
+    const loginUserNameInput = document.getElementById("adminToken");
+    const loginPasswordInput = document.getElementById("passwordToken");
+
+    loginForm.addEventListener("submit", async function (event) {
+      event.preventDefault();
+
+      const userName = loginUserNameInput.value;
+      const password = loginPasswordInput.value;
+
+      const loginFormData = new FormData();
+      loginFormData.append("username", userName);
+      loginFormData.append("password", password);
+
+      await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `username=${encodeURIComponent(
+          userName
+        )}&password=${encodeURIComponent(password)}`,
+      })
+        .then((response) => {
+          if (!response.ok) {
+            alert("Incorrect username or password");
+            
+            throw new Error(
+              "Error al obtener el token de login. Código de estado HTTP: " +
+                response.status
+            );
+          }
+          return response.text(); // Leer la respuesta como texto
+        })
+        .then((token) => {
+          localStorage.setItem("jwtToken", token.trim()); // Guardar el token JWT en localStorage
+          alert("Token generado correctamente");
+        })
+        .catch((error) => {
+          console.error("Error al obtener el token de login:", error);
+          loginUserNameInput.value = ''; 
+          loginPasswordInput.value = ''; 
+        });
+    });
+  } else if (currentUrl.includes("register.html")) {
+    // Código específico para la página de registro
+    const registerButton = document.getElementById("btnRegister");
+    const registerUserNameInput = document.getElementById("adminUser");
+    const registerPasswordInput = document.getElementById("passwordUser");
+
+    registerButton.addEventListener("click", async function (event) {
+      event.preventDefault();
+
+      const userName = registerUserNameInput.value;
+      const password = registerPasswordInput.value;
+
+      const registerFormData = {
+        username: userName,
+        password: password,
+      };
+
+      await fetch("http://localhost:8080/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(registerFormData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+          alert("Successful Register");
+        })
+        .catch((error) => {
+          console.error("Error en el registro:", error);
+        });
+    });
+  }
+});
--- violetSpringGardening/src/main/java/com/example/violetSpringGardening/domain/repository/AdminRepository.java
+++ violetSpringGardening/src/main/java/com/example/violetSpringGardening/domain/repository/AdminRepository.java
@@ -3,5 +3,6 @@ package com.example.violetSpringGardening.domain.repository;
 import com.example.violetSpringGardening.persistence.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminRepository extends JpaRepository <Admin, Long> {
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByUsername(String username);
 }
